fe59ac2ee6531795e065497b14b95d5f1f8eff40
Vishnuoffl72 <vishnuoffl72@gmail.com>
2024-02-27 09:45:44 +0530

    created seeder function to add sample data to db

---
+++ backend/utils/seeder.js
@@ -0,0 +1,24 @@
+const products = require('../data/product.json')
+const product = require('../models/productModel')
+const connectDatabase = require('../config/database')
+
+const dotenv= require('dotenv')
+
+dotenv.config({path:'backend/config/config.env'})
+connectDatabase()
+
+const seedProducts =async ()=>{
+    try{
+        await product.deleteMany();
+        console.log('Products deleted')
+        await product.insertMany(products)
+        console.log('All products added')
+    }catch(error)
+    {
+        console.log(error.message)
+        
+    }
+    process.exit();
+}
+
+seedProducts();
